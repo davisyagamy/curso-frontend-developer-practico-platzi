a2a092a8ddd363a6798ffbc1536c0a3746339e09
Davis Diaz <davis.diaz@gmail.com>
2023-10-24 17:58:41 -0500

    Clase 21
    Realice parcialmente el menu carrito

--- main.js
+++ main.js
@@ -2,14 +2,34 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuBurguer = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
-menuBurguer.addEventListener('click',toogleMobileMenu)
+menuBurguer.addEventListener('click',toogleMobileMenu);
+menuCarIcon.addEventListener('click',toogleCarAside);
+
+
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
 }
 
 function toogleMobileMenu(){
-    mobileMenu.classList.toggle('inactive')
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if (!isAsideClosed){
+        mobileMenu.classList.add('inactive')
+    }
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toogleCarAside(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+   // aside.classList.toggle('inactive');
+
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive')
+        //abrir el aside
+    }
 }
